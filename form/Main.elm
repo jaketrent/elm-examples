139c2cc20b4d3767e7738bda5050ebe37884c030
jaketrent <trent.jake@gmail.com>
2016-07-15 12:13:11 -0600

    runthrough elm arch examples

---
+++ form/Main.elm
@@ -0,0 +1,61 @@
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+main =
+  Html.beginnerProgram
+    { model = model
+    , view = view
+    , update = update
+    }
+
+type alias Model =
+  { name : String
+  , password : String
+  , passwordAgain : String
+  }
+
+model : Model
+model =
+  Model "" "" ""
+
+type Msg
+  = Name String
+  | Password String
+  | PasswordAgain String
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Name name ->
+      { model | name = name }
+
+    Password password ->
+      { model | password = password }
+
+    PasswordAgain passwordAgain ->
+      { model | passwordAgain = passwordAgain }
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ type' "text", placeholder "Name", onInput Name ] []
+    , input [ type' "password", placeholder "Password", onInput Password ] []
+    , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+    , viewValidation model
+    ]
+
+-- should be cap msg?
+-- TODO: refactor out view
+viewValidation : Model -> Html msg
+viewValidation model =
+  let
+    (color, message) =
+      if model.password == model.passwordAgain then
+        ("green", "OK")
+      else
+        ("red", "Passwords do not match!")
+   in
+     div [ style [("color", color)] ] [ text message ]
